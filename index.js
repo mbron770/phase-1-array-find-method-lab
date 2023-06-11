935d6c15a50b7fbafa68f3f34cc5059a906f5896
mbron770 <mbronfin.tsd@gmail.com>
2023-06-11 14:32:58 -0400

    add superbowlWins function with find

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// function superbowlWin(records) {
+//     const win = records.find((record) => record.result === "W")
+// return win ? win.year : undefined
+//   }
+
+
+
+
+
+
+function superbowlWin(arrays){
+    const win = arrays.find((array) => array.result === "W")
+return win ? win.year : undefined
+}
